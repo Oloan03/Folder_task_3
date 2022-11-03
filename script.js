f4a9f0a912e4c84c4ab5dd4e41c0cbcb1e53177e
Oloan Napitupulu <84931568+Oloan03@users.noreply.github.com>
2022-11-03 23:38:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function contact () {
+    var hide = document.getElementById("sosial")
+    if (hide.style.display === "none") {
+        hide.style.display = "block"
+    } else {
+        hide.style.display = "none";
+    }
+}
